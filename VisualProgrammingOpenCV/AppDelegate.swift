6633c87d6c8b3e290828ccd65005bf1fda39641f
AdachiKenta <k18004kk@aitech.ac.jp>
2020-08-12 12:39:43 +0900

    Initial Commit

---
+++ VisualProgrammingOpenCV/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VisualProgrammingOpenCV
+//
+//  Created by k18004kk on 2020/08/12.
+//  Copyright © 2020 AIT. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VisualProgrammingOpenCV/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VisualProgrammingOpenCV
+//
+//  Created by k18004kk on 2020/08/12.
+//  Copyright © 2020 AIT. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
